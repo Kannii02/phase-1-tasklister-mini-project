f0fe73b7e688b0887e85dd6b084f925716b3381d
kannikarsriburin <i_m_juy-juys@hotmail.co.th>
2025-01-13 20:04:45 -1000

    phase-1-tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const taskInput = document.getElementById("new-task-description").value;
+
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+
+    deleteButton.addEventListener("click", () => {
+      li.remove();
+    });
+
+    if (taskInput.trim() === "") {
+      li.textContent = "";
+    } else {
+      li.textContent = taskInput;
+    }
+
+    li.appendChild(deleteButton);
+    taskList.appendChild(li);
+
+    form.reset();
+  });
 });
